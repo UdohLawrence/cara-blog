18005b432e248ee23569a2148583a82972e3f3b6
UdohLawrence <lawrenceaudoh@gmail.com>
2024-04-20 14:06:41 +0100

    navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import React from "react";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
-    <>
-    
-    </>
-  )
+    <div className="App">
+      <NavBar />
+      <div className="content"></div>
+    </div>
+  );
 }
 
-export default App
+export default App;
