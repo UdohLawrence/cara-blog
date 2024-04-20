9a37a53ae670a74f1ed6f6fdac029fa893825059
UdohLawrence <lawrenceaudoh@gmail.com>
2024-04-20 21:15:14 +0100

    fetch data from a local database

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,25 +1,34 @@
 import BlogList from "./BlogList";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const Home = () => {
-    const [blogs, setBlogs] = useState([
-        {
-            author: "Ekomobong",
-            title: "Best of Winters",
-            body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi, mollitia.",
-            id:1
-        },
-        {
-            author: "Lawrence",
-            title: "Strand of Lint",
-            body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi, mollitia.",
-            id:2
-        }
-    ]
-);
+    const [blogs, setBlogs] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        fetch('http://localhost:8080/blogs')
+            .then(res => {
+                if (!res.ok){
+                    throw Error ("Could not fetch data from that resource");
+                }
+                return res.json();
+            })
+            .then(data => {
+                console.log(data);
+                setBlogs(data);
+                setIsLoading(false)
+                setError(null)
+            }).catch(err => {
+                setError(err.message);
+                setIsLoading(false)
+            })
+    }, []);
     return(
         <div className="home">
-            <BlogList blogs={blogs} title="All Blogs!"/>
+            {error && <div> {error}</div>}
+            {isLoading && <h4>Loading....</h4>}
+            {blogs && <BlogList blogs={blogs} title="All Blogs!"/>}
         </div>
     );
 }
