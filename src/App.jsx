e468a82b8256e6a673e751e79d67b046110e3e98
UdohLawrence <lawrenceaudoh@gmail.com>
2024-04-20 14:34:28 +0100

    navbar and navbar styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import NavBar from "./components/NavBar";
+import Home from "./components/Home";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
-      <div className="content"></div>
+      <div className="content">
+        <Home />
+      </div>
     </div>
   );
 }
