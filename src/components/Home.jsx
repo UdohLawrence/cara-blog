f054c03d014630f47ff979b4ea4c4d52c5950d49
UdohLawrence <lawrenceaudoh@gmail.com>
2024-04-20 20:28:33 +0100

    created and used props

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,33 +1,25 @@
+import BlogList from "./BlogList";
 import { useState } from "react";
 
 const Home = () => {
-
     const [blogs, setBlogs] = useState([
-            {
-                author: "Ekomobong",
-                title: "Best of Winters",
-                body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi, mollitia.",
-                id:1
-            },
-            {
-                author: "Lawrence",
-                title: "Strand of Lint",
-                body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi, mollitia.",
-                id:2
-            }
-        ]
-    );
+        {
+            author: "Ekomobong",
+            title: "Best of Winters",
+            body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi, mollitia.",
+            id:1
+        },
+        {
+            author: "Lawrence",
+            title: "Strand of Lint",
+            body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi, mollitia.",
+            id:2
+        }
+    ]
+);
     return(
         <div className="home">
-            <h2>Home component</h2>
-            <div className="articles">
-            {blogs.map((blog) => (
-                <div className="blog-preview" key={blog.id}>
-                    <h3>{blog.title}</h3>
-                    <p>Written by {blog.author}</p>
-                </div>
-            ))}
-            </div>
+            <BlogList blogs={blogs} title="All Blogs!"/>
         </div>
     );
 }
