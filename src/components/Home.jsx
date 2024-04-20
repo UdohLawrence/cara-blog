98c29a825884ddbe62e557e7c0689e250ba90053
UdohLawrence <lawrenceaudoh@gmail.com>
2024-04-20 22:21:43 +0100

    use the custom hook to fetch dat in the home component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,34 +1,14 @@
 import BlogList from "./BlogList";
 import { useState, useEffect } from "react";
+import useFetch from "./useFetch";
 
 const Home = () => {
-    const [blogs, setBlogs] = useState([]);
-    const [isLoading, setIsLoading] = useState(true);
-    const [error, setError] = useState(null);
-
-    useEffect(() => {
-        fetch('http://localhost:8080/blogs')
-            .then(res => {
-                if (!res.ok){
-                    throw Error ("Could not fetch data from that resource");
-                }
-                return res.json();
-            })
-            .then(data => {
-                console.log(data);
-                setBlogs(data);
-                setIsLoading(false)
-                setError(null)
-            }).catch(err => {
-                setError(err.message);
-                setIsLoading(false)
-            })
-    }, []);
+   const {data, isLoading, error} = useFetch('http://localhost:8080/blogs');
     return(
         <div className="home">
             {error && <div> {error}</div>}
             {isLoading && <h4>Loading....</h4>}
-            {blogs && <BlogList blogs={blogs} title="All Blogs!"/>}
+            {data && <BlogList blogs={data} title="All Blogs!"/>}
         </div>
     );
 }
