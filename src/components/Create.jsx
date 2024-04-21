5603c158bb2ca4a544e68444292af0482fe5ce8d
UdohLawrence <lawrenceaudoh@gmail.com>
2024-04-21 08:33:22 +0100

    Redirects to the Home-page, after adding a blog

--- src/components/Create.jsx
+++ src/components/Create.jsx
@@ -1,10 +1,12 @@
 import { useState } from "react";
+import { useHistory } from "react-router-dom/cjs/react-router-dom";
 
 const Create = () => {
   const [title, setTitle] = useState("");
   const [author, setAuthor] = useState("Ekomobong");
   const [body, setBody] = useState("");
   const [isPending, setIsPending] = useState(false);
+  const history = useHistory();
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -20,7 +22,10 @@ const Create = () => {
  }).then(() => {
     console.log('New Blog Added');
     setIsPending(false);
- })
+    history.push('/');
+ });
+
+ 
     
   };
 
