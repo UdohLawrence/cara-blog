d005f3b644e03f76db598aca8cc7b70eff640fb0
UdohLawrence <lawrenceaudoh@gmail.com>
2024-04-21 08:42:59 +0100

    delete functionality

--- src/components/BlogDetails.jsx
+++ src/components/BlogDetails.jsx
@@ -1,10 +1,20 @@
 import { useParams } from "react-router-dom/cjs/react-router-dom";
 import useFetch from "./useFetch";
+import { useHistory } from "react-router-dom/cjs/react-router-dom";
 
 const BlogDetails = () => {
+  const history = useHistory();
   const { id } = useParams();
 
   const {data: blog, error, isLoading} = useFetch('http://localhost:8080/blogs/' + id);
+
+  const handleDelete = () => {
+    fetch('http://localhost:8080/blogs/' + blog.id, {
+      method:'DELETE'
+    }).then(() => {
+      history.push('/')
+    })
+  }
   return (
     <div className="blog-details">
       {isLoading && <div>Loading...</div>}
@@ -14,6 +24,7 @@ const BlogDetails = () => {
             <h2>{blog.title}</h2>
             <p>Written by {blog.author}</p>
             <div>{blog.body}</div>
+            <button onClick={handleDelete}>Delete</button>
         </article>
       )}
     </div>
